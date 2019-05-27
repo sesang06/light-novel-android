e36cfa0a84f833f41eecc5e59ecea9bc7a5f61ae
sesang06 <sesang06@naver.com>
2019-05-27 23:53:16 +0900

    무한 스크롤링 구현

--- app/src/main/java/com/sesang06/lightnovellist/controller/LightNovelListFragment.kt
+++ app/src/main/java/com/sesang06/lightnovellist/controller/LightNovelListFragment.kt
@@ -1,9 +1,12 @@
 package com.sesang06.lightnovellist.controller
 
 import android.arch.lifecycle.ViewModelProviders
+import android.opengl.Visibility
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,23 +30,41 @@ abstract class LightNovelListFragment : Fragment() {
     }
 
     internal val disposeables = AutoClearedDisposable(this)
-
+    private lateinit var scrollListener: RecyclerView.OnScrollListener
     internal val viewDisposables = AutoClearedDisposable(this, false)
     abstract internal val loadType: LoadType
-
+    private val lastVisibleItemPosition: Int
+        get() = layoutManager.findLastVisibleItemPosition()
     internal val viewModelFactory by lazy {
         LightNovelListViewModelFactory(
             provideLightNovelListApi(), loadType
         )
     }
+    internal val layoutManager by lazy {
+        LinearLayoutManager(this.context)
+    }
     lateinit var viewModel: LightNovelListViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_light_novel, container, false)
-        view.light_novel_recycler_view.layoutManager = LinearLayoutManager(this.context)
+        view.light_novel_recycler_view.layoutManager = layoutManager
         view.light_novel_recycler_view.adapter = this@LightNovelListFragment.adapter
+
 
         viewModel = ViewModelProviders.of(this, viewModelFactory)[LightNovelListViewModel::class.java]
 
+
+        scrollListener = object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val totalItemCount = layoutManager.itemCount
+                if (totalItemCount == layoutManager.findLastVisibleItemPosition() + 1) {
+                    if (viewModel.isLoading.value == false) {
+                        viewModel.loadMore()
+                        recyclerView.removeOnScrollListener(scrollListener)
+                    }
+                }
+            }
+        }
         viewModel.lightNovels
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { items ->
@@ -54,12 +75,24 @@ abstract class LightNovelListFragment : Fragment() {
                         setItems(items)
                     }
                     notifyDataSetChanged()
+                    view.light_novel_recycler_view.addOnScrollListener(scrollListener)
                 }
             }
         viewModel.load()
+        viewModel.isLoading
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { isLoading ->
+                view.light_novel_progress_bar.visibility = when (isLoading) {
+                    true -> View.VISIBLE
+                    false -> View.GONE
+                }
+            }
+
 
         return view
     }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
--- app/src/main/java/com/sesang06/lightnovellist/viewmodel/LightNovelListViewModel.kt
+++ app/src/main/java/com/sesang06/lightnovellist/viewmodel/LightNovelListViewModel.kt
@@ -1,37 +1,70 @@
 package com.sesang06.lightnovellist.viewmodel
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModel
 import com.sesang06.lightnovellist.adapter.LoadType
 import com.sesang06.lightnovellist.model.DataResponse
 import com.sesang06.lightnovellist.model.LightNovel
 import com.sesang06.lightnovellist.model.LightNovelList
 import com.sesang06.lightnovellist.service.LightNovelListServiceApi
+import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.BiFunction
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
+import java.util.*
 
 interface LightNovelListViewModelType {
     val lightNovels: BehaviorSubject<List<LightNovel>>
-//    var message: PublishSubject<String>
+    //    var message: PublishSubject<String>
+    val isLoading: BehaviorSubject<Boolean>
+
+    fun request(lastId: Int): Observable<DataResponse<LightNovelList>>
     fun load(): Disposable
+    fun loadMore(): Disposable
 }
 
-class LightNovelListViewModel(val api: LightNovelListServiceApi, val loadType: LoadType): ViewModel(), LightNovelListViewModelType {
+class LightNovelListViewModel(val api: LightNovelListServiceApi, val loadType: LoadType) : ViewModel(),
+    LightNovelListViewModelType {
     override val lightNovels: BehaviorSubject<List<LightNovel>> = BehaviorSubject.create()
 
-    override fun load(): Disposable {
-        val request = when(loadType) {
-            LoadType.HIT -> api.hit(0)
-            LoadType.RECOMMEND -> api.recommend(0)
-            LoadType.NEW -> api.new(0)
+    override val isLoading: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
+
+    override fun request(lastId: Int): Observable<DataResponse<LightNovelList>> {
+        return when (loadType) {
+            LoadType.HIT -> api.hit(lastId)
+            LoadType.RECOMMEND -> api.recommend(lastId)
+            LoadType.NEW -> api.new(lastId)
         }
-       return request
-           .map { response: DataResponse<LightNovelList> ->
-               response.data.list
-           }
-            .subscribe({ items ->
-                lightNovels.onNext(items)
+    }
+
+    override fun loadMore(): Disposable {
+        val lastId = lightNovels.value?.last()?.id ?: 0
+        return request(lastId)
+            .doOnSubscribe { isLoading.onNext(true) }
+            .doOnTerminate { isLoading.onNext(false) }
+            .map { response: DataResponse<LightNovelList> ->
+                response.data.list
+            }
+            .withLatestFrom(lightNovels, BiFunction { append: List<LightNovel>, current: List<LightNovel> ->
+                current + append
             })
+            .subscribe { items ->
+                lightNovels.onNext(items)
+            }
+
+    }
+
+    override fun load(): Disposable {
+        return request(0)
+            .map { response: DataResponse<LightNovelList> ->
+                response.data.list
+            }
+            .doOnSubscribe { isLoading.onNext(true) }
+            .doOnTerminate { isLoading.onNext(false) }
+            .subscribe { items ->
+                lightNovels.onNext(items)
+            }
 
     }
 }
